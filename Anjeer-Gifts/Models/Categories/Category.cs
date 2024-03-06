f39cd28c1028b5d6db0eb4dbb4d7b922ff844656
ShohsanamAbdujabborova <shohsanam1406@gmail.com>
2024-03-06 14:50:46 +0500

    Added JsonProperty to Ordermodel

--- Anjeer-Gifts/Models/Categories/Category.cs
+++ Anjeer-Gifts/Models/Categories/Category.cs
@@ -1,8 +1,10 @@
 ﻿using Anjeer_Gifts.Models.Commons;
+using Newtonsoft.Json;
 
 namespace Anjeer_Gifts.Models.Categories;
 
 public class Category : Auditable
 {
+    [JsonProperty("name")]
     public string Name { get; set; }
 }
--- Anjeer-Gifts/Models/GiftBoxes/GiftBox.cs
+++ Anjeer-Gifts/Models/GiftBoxes/GiftBox.cs
@@ -1,12 +1,14 @@
 ﻿using Anjeer_Gifts.Models.Commons;
 using Anjeer_Gifts.Models.Products;
+using Newtonsoft.Json;
 using Telegram.Bot.Types;
 
 namespace Anjeer_Gifts.Models.GiftBoxes;
 
 public class GiftBox
 {
+    [JsonProperty("userId")]
     public long UserId { get; set; }
-
+    [JsonProperty("products")]
     public List<Product> Products { get; set; }
 }
