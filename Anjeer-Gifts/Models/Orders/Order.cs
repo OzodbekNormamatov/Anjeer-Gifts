a6d2cd02086d85ca41ed42344b5d6848c5bbccbd
ShohsanamAbdujabborova <shohsanam1406@gmail.com>
2024-03-06 14:43:42 +0500

    Added JsonProperty to GiftBoxmodel

--- Anjeer-Gifts/Models/Orders/Order.cs
+++ Anjeer-Gifts/Models/Orders/Order.cs
@@ -1,12 +1,18 @@
-﻿using Telegram.Bot.Types;
+﻿using Newtonsoft.Json;
+using Telegram.Bot.Types;
 
 namespace Anjeer_Gifts.Models.Orders;
 
 public class Order
 {
+    [JsonProperty("customerId")]
     public long CustomerId { get; set; }
+    [JsonProperty("location")]
     public Location Location { get; set; }
+    [JsonProperty("orderNumber")]
     public int OrderNumber { get; set; }
+    [JsonProperty("totalAmount")]
     public decimal TotalAmount { get; set; }
+    [JsonProperty("date")]
     public DateTime Date { get; set; } = DateTime.UtcNow;
 }
