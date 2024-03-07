bc291414b1818bc6bbbc2f465b28bae1b809a264
Asrorbek <as.abrorov@gmail.com>
2024-03-07 14:48:43 +0500

    Connected to Database

---
+++ Anjeer-Gifts/Database/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Anjeer_Gifts.Models.Orders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anjeer_Gifts.Database;
+
+public class MyDbContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Set the connection string for your PostgreSQL database
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Anjeer-Gifts;Username=postgres;Password=0511");
+    }
+}
